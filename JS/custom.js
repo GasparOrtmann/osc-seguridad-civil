cc5dfa7176430b42d8ebe4d8ba5cd3a895b34518
Gaspar Ortmann <gasparortmann@gmail.com>
2023-05-17 15:51:09 -0300

    a

--- JS/custom.js
+++ JS/custom.js
@@ -33,43 +33,10 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: "#swiper-button-next-1",
         prevEl: "#swiper-button-prev-1",
     },
-});
 
+    autoplay: {
+        delay: 3000,
+      },
 
-
-var swiper = new Swiper(".mySwiper2", {
-    slidesPerView: 2.5,
-    spaceBetween: 10,
-    slidesPerGroup: 1,
-    loop: false,
-    loopFillGroupWithBlank: true,
-    breakpoints: {
-        1000: {
-            slidesPerView: 4,
-            spaceBetween: 20,
-        },
-
-        768: {
-            slidesPerView: 1.5,
-            spaceBetween: 20,
-        },
-        484: {
-            slidesPerView: 1.5,
-            spaceBetween: 20,
-        },
-        280: {
-            slidesPerView: 1.2,
-            spaceBetween: 20,
-        },
-        100: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-        },
-    },
-
-
-    navigation: {
-        nextEl: "#swiper-button-next-2",
-        prevEl: "#swiper-button-prev-2",
-    },
+      loop: true,
 });
