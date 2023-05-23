38651e9d6f92709c6d3c1d9d297a0a87bcd085f8
Gaspar Ortmann <gasparortmann@gmail.com>
2023-05-23 15:57:23 -0300

    btn faq complete

--- JS/desplegable.js
+++ JS/desplegable.js
@@ -2,6 +2,6 @@ const accordion = document.getElementsByClassName('faq__active');
 
 for (i = 0; i<accordion.length; i++){
     accordion[i].addEventListener('click', function(){
-        accordion.classList.toggle('active')
+        this.classList.toggle('active')
     })
 }
