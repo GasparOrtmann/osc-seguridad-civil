373c4d6e4301d5216cc20f51e90c813aeae3618a
Gaspar Ortmann <gasparortmann@gmail.com>
2023-06-01 18:22:53 -0300

    834px resolution

--- JS/nav.js
+++ JS/nav.js
@@ -7,3 +7,20 @@ document.addEventListener('scroll', () => {
         header.classList.remove('scrolled');
     }
 })
+
+
+const nav = document.querySelector("#nav");
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
+const img = document.querySelector("#header");
+const navList = document.querySelector("#nav-list");
+
+open.addEventListener("click", () => {
+    nav.classList.add("visible");
+    img.classList.add("shadow");
+})
+
+navList.addEventListener("click", () => {
+    nav.classList.remove("visible");
+    img.classList.remove("shadow");
+})
