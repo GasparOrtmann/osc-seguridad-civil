d0ff7f3f3c6ecd5f89da9914907d91133316903c
Gaspar Ortmann <gasparortmann@gmail.com>
2023-06-13 11:08:23 -0300

    email php (?

--- sendEmail.php
+++ sendEmail.php
@@ -1,7 +1,39 @@
 <?php
+if (isset($_POST["sendEmailBtn"])){
+    $name = $_POST["name"];
+    $empresa = $_POST["empresa"];
+    $email = $_POST["email"];
+    $telephone = $_POST["telephone"];
+    $message = $_POST["message"];
 
-$name = $_POST["name"];
-$empresa = $_POST["empresa"];
-$email = $_POST["email"];
-$telefono = $_POST["telefono"];
-$mensaje = $_POST["mensaje"];
+    $from = $name;
+    $to = "info@seguridadcivil.com.ar";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: '.$email.'<'.$email.'>' . "\r\n".'Reply-To: '.$email."\r\n" . 'X-Mailer: PHP/' . phpversion();
+    $message = '<!doctype html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport"
+					  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+				<meta http-equiv="X-UA-Compatible" content="ie=edge">
+				<title>Document</title>
+			</head>
+			<body>
+			<span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">'.$message.'</span>
+				<div class="container">
+                 '.$message.'<br/>
+                    Regards<br/>
+                  '.$fromEmail.'<br/>
+                  '.$name.'<br/>
+                  '.$empresa.'<br/>
+                  '.$telephone.'
+				</div>
+			</body>
+			</html>';
+    $result = @mail($to, $from, $message, $headers);
+
+    echo '<script>alert("Email sent successfully !")</script>';
+    echo '<script>window.location.href="index.php";</script>';
+}
